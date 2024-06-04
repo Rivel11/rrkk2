7fb773f8376a866a5d4e24b7257f291505209cf6
Rivel11 <starod.anya5@gmail.com>
2024-06-04 10:20:14 +0300

    Initial commit

---
+++ Program.cpp
@@ -0,0 +1,23 @@
+// ===========================================================================
+// Program.cpp // Command Pattern
+// ===========================================================================
+// function prototypes
+
+#include "ConceptualExample_01.cpp"
+#include "ConceptualExample_02.cpp"
+
+void test_conceptual_example_01();
+void test_conceptual_example_02();
+void test_chess_example();
+
+int main()
+{
+    test_conceptual_example_01();
+    test_conceptual_example_02();
+    test_chess_example();
+    return 0;
+}
+
+// ===========================================================================
+// End-of-File
+// ===========================================================================
